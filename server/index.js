fdd59f438f6c0fd7c01b110e5c7b8c42b283d8ea
Sergey <git config --global  user.email>
2022-01-10 07:13:41 +0200

    eleven

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.use("/rating", require("./routes/ratingRouter"))
 
 const start = async () => {
   try {
-    // await sequelize.authenticate()
-    // await sequelize.sync()
+    await sequelize.authenticate()
+    await sequelize.sync()
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
   } catch (err) {
     console.log(err)
