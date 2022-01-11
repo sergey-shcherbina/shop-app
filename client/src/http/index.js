cbebe977d4a4a95362c755c692ce87db7996cb82
Sergey <git config --global  user.email>
2022-01-11 18:54:14 +0200

    dep th

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "production"
     ? "/"
-    : process.env.REACT_APP_API_URL
+    : "/"
+    
+    // : process.env.REACT_APP_API_URL
 
 const $host = axios.create({
   baseURL
