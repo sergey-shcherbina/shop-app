f1ee22f9efe4bcb6fd84c10639557e9413c30c67
Sergey <git config --global  user.email>
2022-01-11 13:48:43 +0200

    deploy

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -2,21 +2,21 @@ import {$authHost, $host} from "."
 import jwt_decode from "jwt-decode"
 
 export const registration = async (email, password, name) => {
-  const {data} = await $host.post("user/registration", {email, password, name})
+  const {data} = await $host.post("api/user/registration", {email, password, name})
   localStorage.setItem("token", data.token)
   return jwt_decode(data.token)
 }
 export const login = async (email, password) => {
-  const {data} = await $host.post("user/login", {email, password})
+  const {data} = await $host.post("api/user/login", {email, password})
   localStorage.setItem("token", data.token)
   return jwt_decode(data.token)
 }
 export const check = async () => {
-  const {data} = await $authHost.get("user/auth")
+  const {data} = await $authHost.get("api/user/auth")
   localStorage.setItem("token", data.token)
   return jwt_decode(data.token)
 }
 export const fetchUsers = async () => {
-  const {data} = await $authHost.get("/user")
+  const {data} = await $authHost.get("api//user")
   return data
 }
