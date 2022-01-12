0a2331c9369dd88a8f54850918e25f8ce5d7eb46
Sergey <git config --global  user.email>
2022-01-12 11:55:57 +0200

    cosh

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const fileUpload = require("express-fileupload")
 const path = require("path")
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3003
 const app = express()
 app.use(cors())
 app.use(express.json())
