718b7f581666bba8656064d670a7f290aa69b806
Sergey <git config --global  user.email>
2022-01-14 08:40:03 +0200

    ini

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import AppRouter from "./components/AppRouter"
 import NavBar from "./components/NavBar"
 import {Context} from "."
 import {baseURL} from "./http"
+import {observer} from "mobx-react-lite"
 
-const App = () => {
+const App = observer(() => {
   const {store} = useContext(Context)
+  console.log(store.backgr.img)
   return (
     <div 
       // style={{background:`url(${process.env.REACT_APP_API_URL + (store.backgr.img || "1000.jpg")}) no-repeat`,
@@ -19,7 +21,7 @@ const App = () => {
       </BrowserRouter>
     </div>
   )
-}
+})
 
 export default App
 
